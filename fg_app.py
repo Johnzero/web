aa9c9f87eb08b1b24c01fb571b3adbe8a6ae42a7
Daniel Yang <daniel.yang.zhenyu@gmail.com>
2012-08-23 16:35:31 +0800

    more more.

--- fg_app.py
+++ fg_app.py
@@ -8,14 +8,11 @@ Copyright (c) 2012 Fu Guang Industrial Co., Lmt.. All rights reserved.
 """
 #Sqlalchemy, Admin, Cache, Login, WTF, WTForms, Mail, 
 
-import sys, os, datetime, uuid, simplejson as json
-from datetime import date
+import sys, os, uuid, simplejson as json
+from datetime import date, datetime
 
-from flask import Flask, session, request, render_template, redirect, url_for, flash, send_from_directory
+from flask import Flask, session, request, render_template, redirect, url_for, flash, send_from_directory, views
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.admin import Admin
-from flask.ext.admin.contrib.fileadmin import FileAdmin
-from flask.ext.admin.contrib.sqlamodel import ModelView
 
 from flask.ext.login import (LoginManager, current_user, login_required,
                             login_user, logout_user, UserMixin, AnonymousUser,
@@ -29,16 +26,9 @@ except ImportError:
     from simplejson import dumps
 
 #------------------------------------------------------------------------------------------------------------
-def jsonify(f):
-    """返回json"""
-    def inner(*args, **kwargs):
-        return Response(dumps(f(*args, **kwargs)), mimetype='application/json')
-    return inner
-
-
-#------------------------------------------------------------------------------------------------------------
+#配置定义
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fuguang:fuguang@localhost:5432/fuguang'
 
@@ -46,25 +36,18 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['SECRET_KEY'] = 'zuSAyu3XRqGRvAg0HxsKX12Nrvf6Hk3AgZCWg1S1j9Y='
 
-
 #upload file extensions
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 #thumb
 thumb_size = 250, 200
 
-#admin = Admin(app, name='FG Admin')
-#path = os.path.join(os.path.dirname(__file__), 'static', 'upload')
-#admin.add_view(FileAdmin(path, '/static/upload/', name='Files'))
-
-#helper
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
 #------------------------------------------------------------------------------------------------------------
+#用户认证
+db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
     roles = db.Column(db.String(100))
     active = db.Column(db.Boolean())
@@ -95,17 +78,65 @@ def load_user(userid):
     return User.query.get(userid)
 
 #------------------------------------------------------------------------------------------------------------
+#工具
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def path2url(path):
+    return '/%s/' % ('/'.join(path.split('\\')))
+
+def jsonify(f):
+    """返回json"""
+    def inner(*args, **kwargs):
+        return Response(dumps(f(*args, **kwargs)), mimetype='application/json')
+    return inner
+
+@app.context_processor
+def active_processor():
+    def page_active(code_list, path):
+        base_name = os.path.basename(path)
+        
+        return base_name in code_list
+    
+    return dict(page_active=page_active)
+
+#------------------------------------------------------------------------------------------------------------
+#模型
+
+class Page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(50), unique=True)
+    title = db.Column(db.String(50))
+    keyword = db.Column(db.Text())
+    content = db.Column(db.Text())
+    created = db.Column(db.DateTime(), default=datetime.now)
+    updated = db.Column(db.DateTime(), onupdate=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __init__(self, user_id, code, title, keyword, content):
+        self.code = code
+        self.user_id = user_id
+        self.content = content
+        self.title = title
+        self.keyword = keyword
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    news_list = db.relationship('News', backref='category', lazy='dynamic')
+    news_list = db.relationship('News', backref='category', order_by='desc(News.created)', lazy='dynamic')
+    
+    def __init__(self, name):
+        self.name = name
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     content = db.Column(db.Text())
+    created = db.Column(db.DateTime(), default=datetime.now)
+    updated = db.Column(db.DateTime(), onupdate=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 product2tag = db.Table('product_tag',
@@ -127,55 +158,173 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
-# admin.add_view(ModelView(User, db.session))
-# admin.add_view(ModelView(Category, db.session))
-# admin.add_view(ModelView(News, db.session))
-# admin.add_view(ModelView(Product, db.session))
-# admin.add_view(ModelView(Tag, db.session))
+
+class ResellerCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    reseller_list = db.relationship('Reseller', backref='category', lazy='dynamic')
+    
+    def __init__(self, name):
+        self.name = name
+
+class Reseller(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    category_id = db.Column(db.Integer, db.ForeignKey('reseller_category.id'))
+    province = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    geo = db.Column(db.String(50))
+    telephone = db.Column(db.String(50))
 
 #------------------------------------------------------------------------------------------------------------
-
+#首页
 @app.route('/')
 def index():
-	return render_template('index.html')
+        return render_template('index.html')
 
-@app.route("/login", methods=["GET", "POST"])
+#------------------------------------------------------------------------------------------------------------
+#页面
+
+@app.route('/page/<string:code>')
+def page(code):
+    item = Page.query.filter_by(code=code).first_or_404()
+    
+    return render_template("page.html", page=item)
+
+#------------------------------------------------------------------------------------------------------------
+#旗下品牌
+
+@app.route('/brand/<string:name>')
+def brand(name):
+    return render_template("brand/%s.html" % name)
+
+#------------------------------------------------------------------------------------------------------------
+#新闻动态-分页
+@app.route('/news', defaults={'page': 1})
+@app.route('/news/p/<int:page>')
+def news(page):
+    pass
+
+#查看新闻
+@app.route('/news/view/<int:news_id>')
+def news_view(news_id):
+    pass
+
+#------------------------------------------------------------------------------------------------------------
+#产品中心
+@app.route('/product', defaults={'page': 1})
+@app.route('/product/p/<int:page>')
+def product(page):
+    pass
+
+@app.route('/product/view/<int:product_id>')
+def product_view(product_id):
+    pass
+
+@app.route('/product/category', defaults={'page': 1})
+@app.route('/product/category/<int:category_id>/<int:page>')
+def product_category(category_id, page):
+    pass
+
+@app.route('/product/tag', defaults={'page': 1})
+@app.route('/product/tag/<int:tag_id>/<int:page>')
+def product_tag(tag_id, page):
+    pass
+
+#------------------------------------------------------------------------------------------------------------
+#销售渠道
+
+@app.route('/sales/category', defaults={'page': 1})
+@app.route('/sales/category/<int:category_id>/<int:page>')
+def sales():
+    pass
+
+#------------------------------------------------------------------------------------------------------------
+#客户服务
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+
+#------------------------------------------------------------------------------------------------------------
+# 用户
+
+@app.route('/user/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST" and "username" in request.form:
-        username = request.form["username"]
-        password = request.form["password"]
+    if request.method == 'POST' and 'username' in request.form:
+        username = request.form['username']
+        password = request.form['password']
         
         user_in_db = User.query.filter_by(name=username, password=password).first()
         
         if user_in_db:
-            remember = request.form.get("remember", "no") == "yes"
+            remember = request.form.get('remember', 'no') == 'yes'
             if login_user(user_in_db, remember=remember):
-                flash("Logged in!")
-                return redirect(request.args.get("next") or url_for("index"))
+                flash('Logged in!')
+                return redirect(request.args.get('next') or url_for('index'))
             else:
-                flash("Sorry, but you could not log in.")
+                flash('Sorry, but you could not log in.')
         else:
-            flash(u"Invalid username or password.")
-    return render_template("login.html")
+            flash(u'Invalid username or password.')
+    return render_template('login.html')
 
-@app.route("/logout")
+@app.route('/user/logout')
 @login_required
 def logout():
     logout_user()
-    flash("Logged out.")
-    return redirect(url_for("index"))
+    flash('Logged out.')
+    return redirect(url_for('index'))
+
+@app.route('/user/reauth', methods=['GET', 'POST'])
+@login_required
+def reauth():
+    if request.method == 'POST':
+        confirm_login()
+        flash(u'Reauthenticated.')
+        return redirect(request.args.get('next') or url_for('index'))
+    return render_template('reauth.html')
 
 #------------------------------------------------------------------------------------------------------------
-@app.context_processor
-def get_urlmap():
-    for url in app.url_map:
-        print url
-    return dict(urlmap=[])
+#文本编辑器-文件上传
+@app.route('/rd/upload', methods=['GET', 'POST'])
+@jsonify
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            folder_name = date.today().strftime('%Y-%m-%d')
+            dir = os.path.join(app.config['UPLOAD_FOLDER'], folder_name)
+            thumb_dir = os.path.join(app.config['UPLOAD_FOLDER'], folder_name, 'thumb')
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            if not os.path.exists(thumb_dir):
+                os.makedirs(thumb_dir)
+            
+            extension = os.path.splitext(file.filename)[1]
+            filename = str(uuid.uuid4()) + extension
+            file.save(os.path.join(dir, filename))
+            #thumb
+            im = Image.open(os.path.join(dir, filename))
+            im.thumbnail(thumb_size)
+            im.save(os.path.join(thumb_dir, filename), "JPEG")
+            
+            return {'filelink':'/static/upload/%s/%s' % (folder_name, filename)}
+    else:
+        file_list = []
+        for root, dirs, files in os.walk(app.config['UPLOAD_FOLDER']):
+            if dirs:
+                url_prefix = path2url(root)
+                for f in files:
+                    file_list.append({
+                        'thumb': url_prefix + 'thumb/' + f,
+                        'image': url_prefix + f,
+                        'folder': url_prefix,
+                    })
 
+        return file_list
 
 #------------------------------------------------------------------------------------------------------------
-def create_db():
-    return db.create_all()
 
 def run():
     app.config.from_object(__name__)
@@ -183,7 +332,33 @@ def run():
     
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        if sys.argv[1] == 'run':
-            run()
-        elif sys.argv[1] == 'create_db':
-            create_db()
+        if sys.argv[1] == 'syncdb':
+            db.create_all()
+        elif sys.argv[1] == 'dropdb':
+            db.drop_all() 
+        elif sys.argv[1] == 'initdb':
+            #init db
+            db.session.add(User('admin','admin', 'admin', True))
+            
+            db.session.add(Category('新闻'))
+            db.session.add(Category('公告'))
+            db.session.add(Category('首页大幅'))
+            
+            db.session.add(ResellerCategory('直营店'))
+            db.session.add(ResellerCategory('批发'))
+            db.session.add(ResellerCategory('FGA'))
+            db.session.add(ResellerCategory('网络'))
+            
+            db.session.commit()
+            
+            
+            db.session.add(Page(1, 'fuguang', '关于富光', '富光', 'fuguang'))
+            db.session.add(Page(1, 'history', '富光历史', '富光', 'history'))
+            db.session.add(Page(1, 'philosophy', '理念','富光','philosophy'))
+            db.session.add(Page(1, 'honor', '荣誉资质', '富光', 'honor'))
+            
+            db.session.commit()
+        else:
+            print 'unkown command. support sycndb, initdb, dropdb.'
+    else:
+        run()
